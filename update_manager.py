5eb1d6328599ac8130c088708623a171051ca811
Ethan Rubenstein <ethanmrubenstein@gmail.com>
2025-03-24 11:20:54 -0400

    Added update_mananger.py and edited main.py to only contain main logic.

---
+++ update_manager.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+
+REPO_DIR = "../git-test"
+
+def update_code():
+    cwd = os.getcwd()
+    os.chdir(REPO_DIR)
+
+    subprocess.run(["git", "fetch"], check=True)
+    result = subprocess.run(["git", "rev-list", "HEAD...origin/main", "--count"], capture_output=True, text=True)
+
+    if result.stdout.strip() != "0":
+        print("[+] Update found. Pulling latest version...")
+        subprocess.run(["git", "reset", "--hard", "origin/main"], check=True)
+        subprocess.run(["git", "clean", "-fd"], check=True)
+    else:
+        print("[+] Already up to date.")
+
+    os.chdir(cwd)
+
+update_code()
